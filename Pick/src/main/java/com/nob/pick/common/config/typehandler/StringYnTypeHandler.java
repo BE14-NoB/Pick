e3832ba7989875d9c75d0e60cd5b276362c05df9
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2025-03-21 16:38:03 +0900

    Refactor: TypeHandler implements -> extends

--- Pick/src/main/java/com/nob/pick/common/config/typehandler/StringYnTypeHandler.java
+++ Pick/src/main/java/com/nob/pick/common/config/typehandler/StringYnTypeHandler.java
@@ -1,5 +1,6 @@
 package com.nob.pick.common.config.typehandler;
 
+import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
 import org.apache.ibatis.type.TypeHandler;
 
@@ -8,27 +9,27 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class StringYnTypeHandler implements TypeHandler<Boolean> {
+public class StringYnTypeHandler extends BaseTypeHandler<Boolean> {
 
     @Override
-    public void setParameter(PreparedStatement ps, int i, Boolean parameter, JdbcType jdbcType) throws SQLException {
+    public void setNonNullParameter(PreparedStatement ps, int i, Boolean parameter, JdbcType jdbcType) throws SQLException {
         ps.setString(i, parameter ? "Y" : "N");
     }
 
     @Override
-    public Boolean getResult(ResultSet rs, String columnName) throws SQLException {
+    public Boolean getNullableResult(ResultSet rs, String columnName) throws SQLException {
         String s = rs.getString(columnName);
         return "Y".equalsIgnoreCase(s);
     }
 
     @Override
-    public Boolean getResult(ResultSet rs, int columnIndex) throws SQLException {
+    public Boolean getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
         String s = rs.getString(columnIndex);
         return "Y".equalsIgnoreCase(s);
     }
 
     @Override
-    public Boolean getResult(CallableStatement cs, int columnIndex) throws SQLException {
+    public Boolean getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
         String s = cs.getString(columnIndex);
         return "Y".equalsIgnoreCase(s);
     }
