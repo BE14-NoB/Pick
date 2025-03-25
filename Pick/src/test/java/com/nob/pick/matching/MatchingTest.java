796634751b2c0785f8f05c1a7ab9970a8c326f83
swjang7269 <azamiyuko7269@gmail.com>
2025-03-25 15:20:51 +0900

    test: 매칭 기능 테스트

--- Pick/src/test/java/com/nob/pick/matching/MatchingTest.java
+++ Pick/src/test/java/com/nob/pick/matching/MatchingTest.java
@@ -1,14 +1,22 @@
 package com.nob.pick.matching;
 
 import com.nob.pick.matching.query.dto.MatchingDTO;
+import com.nob.pick.matching.query.dto.SearchMatchingDTO;
 import com.nob.pick.matching.query.service.MatchingService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
+import java.util.stream.Stream;
 
 @SpringBootTest
 @Transactional
@@ -16,9 +24,8 @@ public class MatchingTest {
     @Autowired
     private MatchingService matchingService;
 
-
     @Test
-    @DisplayName("전체 매칭방 조회")
+    @DisplayName("전체 매칭방 조회 테스트")
     public void findAllMatching() {
         List<MatchingDTO> findAllMatching = matchingService.getMatching();
         if(findAllMatching.size() > 0) {
@@ -27,4 +34,19 @@ public class MatchingTest {
             System.out.println("Not found Matching");
         }
     }
+
+    private static Stream<Arguments> getSearchMatchingInfo() {
+        SearchMatchingDTO searchMatchingInfo = new SearchMatchingDTO();
+        searchMatchingInfo.setMemberId(4);
+        searchMatchingInfo.setTechnologyCategoryCode(1);
+        return Stream.of(Arguments.arguments(searchMatchingInfo));
+    }
+
+    @DisplayName("매칭 기능 테스트")
+    @ParameterizedTest
+    @MethodSource("getSearchMatchingInfo")
+    public void searchMatching(SearchMatchingDTO searchMatchingInfo) {
+        Assertions.assertDoesNotThrow(() -> matchingService.getSearchMatching(searchMatchingInfo));
+    }
+
 }
