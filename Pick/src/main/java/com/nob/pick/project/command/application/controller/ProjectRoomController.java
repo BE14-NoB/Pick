989de0bb7c4280d9d1aac7b03eafb8ac77045207
minsun24 <jms49526@gmail.com>
2025-04-06 09:32:32 +0900

    [project] 프로젝트 도메인에서만 member 연결 분리 (#312)

--- Pick/src/main/java/com/nob/pick/project/command/application/controller/ProjectRoomController.java
+++ Pick/src/main/java/com/nob/pick/project/command/application/controller/ProjectRoomController.java
@@ -2,6 +2,7 @@ package com.nob.pick.project.command.application.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +29,6 @@ public class ProjectRoomController {
 	 * 	세션 코드 없음.
 	 * 	팀원 맵핑 필요. (공통)
 	 * */
-
 	@PostMapping("/matching")
 	public ResponseEntity<?> registMathingProjectRoom(@RequestBody RequestProjectRoomDTO newProjectRoom) {
 		// 세션 코드 없음
@@ -47,6 +47,21 @@ public class ProjectRoomController {
 		projectRoomService.createNonMatchingProject(newProjectRoom);
 		return ResponseEntity.ok().build();
 	}
+
+	// TODO. 프로젝트 방 삭제
+	
+
+
+	
+	/* TODO. 프로젝트 방 정보 수정
+		- 프로젝트 정보(프로젝트명, 한줄 소개, 설명, 썸네일)
+	*/
+
+
+
+
+
+
 
 }
 
