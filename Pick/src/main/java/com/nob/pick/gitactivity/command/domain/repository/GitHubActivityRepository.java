c86a6430119ab7ef463985354435abf16f9b2da6
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2025-04-07 16:05:18 +0900

    Refactor: DB 저장 변경으로 인한 안쓰는 파일 삭제

--- Pick/src/main/java/com/nob/pick/gitactivity/command/domain/repository/GitHubActivityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nob.pick.gitactivity.command.domain.repository;
-
-import com.nob.pick.gitactivity.command.domain.aggregate.GitHubAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GitHubActivityRepository extends JpaRepository<GitHubAccount, Integer> {
-}
