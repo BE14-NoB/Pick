daf231e5f25342b99d0bec26b54c02a2a5e22e59
swjang7269 <azamiyuko7269@gmail.com>
2025-04-08 16:06:43 +0900

    Test: 매칭 관련 테스트

--- Pick/src/test/java/com/nob/pick/matching/MatchingTest.java
+++ Pick/src/test/java/com/nob/pick/matching/MatchingTest.java
@@ -2,7 +2,9 @@ package com.nob.pick.matching;
 
 
 import com.nob.pick.matching.query.dto.MatchingDTO;
+import com.nob.pick.matching.query.dto.MatchingEntryDTO;
 import com.nob.pick.matching.query.dto.SearchMatchingDTO;
+import com.nob.pick.matching.query.dto.TechnologyCategoryDTO;
 import com.nob.pick.matching.query.service.MatchingService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -15,25 +17,74 @@ import java.util.List;
 public class MatchingTest {
     @Autowired
     private MatchingService matchingService;
-    private com.nob.pick.matching.command.service.MatchingService commandService;
 
     @Test
     @DisplayName("전체 매칭방 조회 테스트")
     public void findAllMatchingTest() {
-        Assertions.assertDoesNotThrow(() -> matchingService.getMatching());
+        List<MatchingDTO> result = matchingService.getMatching();
+        result.forEach(System.out::println);
     }
 
+    @Test
+    @DisplayName("매칭방 id로 매칭방 조회")
+    public void findMatchingByMatchingIdTest() {
+        int matchingId = 4;
+        List<MatchingDTO> result = matchingService.getMatchingByMatchingId(matchingId);
+        result.forEach(System.out::println);
+    }
     @Test
     @DisplayName("기술 카테고리 id로 매칭방 조회 테스트")
     public void findMatchingByTechnologyCategoryIdTest() {
         int technologyCategoryId = 4;
-        Assertions.assertDoesNotThrow(() -> matchingService.getMatchingByTechnologyCategoryId(technologyCategoryId));
+        List<MatchingDTO> result = matchingService.getMatchingByTechnologyCategoryId(technologyCategoryId);
+        result.forEach(System.out::println);
     }
 
     @Test
-    @DisplayName("매칭방 id로 신청자 조회")
+    @DisplayName("매칭방 id로 신청자 조회 테스트")
     public void findMatchingEntryByMatchingIdTest() {
         int matchingId = 7;
-        Assertions.assertDoesNotThrow(() -> matchingService.getMatchingEntryByMatchingId(matchingId, false));
+        List<MatchingEntryDTO> result = matchingService.getMatchingEntryByMatchingId(matchingId, false);
+        result.forEach(System.out::println);
     }
+
+    @Test
+    @DisplayName("기술 카테고리 조회 테스트")
+    public void findTechnologyCategoryTest() {
+        List<TechnologyCategoryDTO> result = matchingService.getTechnologyCategory();
+        result.forEach(System.out::println);
+    }
+
+    @Test
+    @DisplayName("기술 카테고리 id로 기술 카테고리 조회 테스트")
+    public void findTechnologyCategoryByTechnologyCategoryIdTest() {
+        int technologyCategoryId = 7;
+        List<TechnologyCategoryDTO> result = matchingService.getTechnologyCategoryByTechnologyCategoryId(technologyCategoryId);
+        result.forEach(System.out::println);
+    }
+
+    @Test
+    @DisplayName("상위 기술 카테고리로 하위 기술 카테고리 조회 테스트")
+    public void findSubTechnologyCategoryByRefTechnologyCategoryIdTest() {
+        int refTechnologyCategoryId = 3;
+        List<TechnologyCategoryDTO> result = matchingService.getSubTechnologyCategoryByRefTechnologyCategoryId(refTechnologyCategoryId);
+        result.forEach(System.out::println);
+    }
+
+    @Test
+    @DisplayName("상위 기술 카테고리 조회 테스트")
+    public void findParentTechnologyCategoryTest() {
+        List<TechnologyCategoryDTO> result = matchingService.getParentTechnologyCategory();
+        result.forEach(System.out::println);
+    }
+
+
+    @Test
+    @DisplayName("방장 id로 매칭방 조회")
+    public void findMatchingByManagerIdTest() {
+        int managerId = 2;
+        List<MatchingDTO> result = matchingService.getMatchingByManagerId(managerId);
+        result.forEach(System.out::println);
+    }
+
 }
