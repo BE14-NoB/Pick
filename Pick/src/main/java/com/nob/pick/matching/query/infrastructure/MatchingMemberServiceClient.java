1612dba9cefa9565ddace2230dff3d0093cbc120
swjang7269 <azamiyuko7269@gmail.com>
2025-04-08 17:40:55 +0900

    refactor: 멤버 클라이언트 반환 형식에 맞춰 리팩토링 및 클라이언트 컨텍스트id 지정

--- Pick/src/main/java/com/nob/pick/matching/query/infrastructure/MatchingMemberServiceClient.java
+++ Pick/src/main/java/com/nob/pick/matching/query/infrastructure/MatchingMemberServiceClient.java
@@ -2,16 +2,17 @@ package com.nob.pick.matching.query.infrastructure;
 
 import com.nob.pick.common.config.infrastructure.FeignClientConfig;
 import com.nob.pick.matching.query.vo.ResponseMemberProfileVO;
-import com.nob.pick.matching.query.vo.ResponseMemberVO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="matching-pick-member-service", configuration= FeignClientConfig.class)
+import java.util.Map;
+
+@FeignClient(name="pick-member-service", contextId = "matching2MemberClient", configuration= FeignClientConfig.class)
 public interface MatchingMemberServiceClient {
     // 현재 로그인한 회원 정보 가져오기
-    @GetMapping("/api/members/member-Info/id")
-    ResponseMemberVO getMemberId();
+    @GetMapping("/api/members/user-info")
+    Map<String, Object> getUserInfo();
 
     // 멤버 id로 해당 회원 프로필 조회
     @GetMapping("/api/members/profile-page/{memberId}")
